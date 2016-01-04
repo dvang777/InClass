37e5e1e74c5c68ed9cea538a6fd56593258c042c
Daniel Vang <danielvang@ghalliance.org>
2016-01-04 17:27:19 -0600

    added function to js

--- js files/wageplusot.js
+++ js files/wageplusot.js
@@ -31,3 +31,18 @@ console.log("workerbhours=", workerbhours);
 console.log("otpay=", otpay);
 console.log("othour=", othour);
 console.log("(workerbpay * 40) + (otpay * othour)=", workerbtotal);
+
+function addwage(pay,hours){
+	var hours = prompt ("Please enter hours", " ");
+	var pay = 23.43;
+	var result = hours * pay;
+	result = pay * hours;
+	return result;
+	}
+
+var pay = 23.43;
+var hours = 40;
+var total = pay * hours;
+total = addwage(pay, hours);
+console.log(total);
+
