4d8bb0b4b5f202d8153a2c596b65ec35ced379e9
Daniel Vang <danielvang@ghalliance.org>
2016-01-04 12:35:45 -0600

    added new file wageplusot

---
+++ js files/wageplusot.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var workerapay;
+var workerahours;
+var workeratotal;
+
+workerapay = 17.24;
+workerahours = 34
+workeratotal = workerapay * workerahours
+
+console.log("workerapay=", workerapay);
+console.log("workerahours=", workerahours);
+console.log("workerapay*workerahours=", workeratotal)
+
+
+var workerbpay;
+var workerbhours;
+var otpay;
+var othour;
+var workerbtotal;
+
+workerbpay = 23.43;
+workerbhours = 47;
+
+
+
+if(workerbhours > 40){
+	otpay = 1.5 * workerbpay;
+	othour = workerbhours - 40;
+}
+workerbtotal = (workerbpay * workerbhours) + (otpay * othour)
+
+console.log("workerbpay=", workerbpay);
+console.log("workerbhours=", workerbhours);
+console.log("otpay=", otpay);
+console.log("othour=", othour);
+console.log("(workerbpay * workerbhours) + (otpay * othour)", workerbtotal);
