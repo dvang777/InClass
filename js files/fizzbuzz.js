fc58d4d51e8c9028938858978499fd1848247604
Daniel Vang <danielvang@ghalliance.org>
2016-01-05 11:45:49 -0600

    added missing curly braces in fizzbuzz.js

--- js files/fizzbuzz.js
+++ js files/fizzbuzz.js
@@ -1,8 +1,8 @@
 "use strict";
 for (var i = 0; i <= 100; i++) {
-	if (i%3 + i%5 === 0)
-		console.log("fizzbuzz")
-	    else if (i%3 === 0) {
+	if (i%3 + i%5 === 0){
+		console.log("fizzbuzz");
+	    } else if (i%3 === 0) {
 		    console.log("fizz");
 	        } else if (i%5 === 0){
 		        console.log("buzz");
