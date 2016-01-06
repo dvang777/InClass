a0590d0e74ebdb62a98a4478a97791ceede75802
Daniel Vang <danielvang@ghalliance.org>
2016-01-06 16:43:05 -0600

    fixed wage calc

--- js files/wageplusot.js
+++ js files/wageplusot.js
@@ -32,41 +32,43 @@ console.log("otpay=", otpay);
 console.log("othour=", othour);
 console.log("(workerbpay * 40) + (otpay * othour)=", workerbtotal);
 
+
 function regwage(reghours,regpay){
 	var wage;
 	wage = reghours * regpay;
 	return wage;
     }
-var hours;
-var pay;
-var totalreg;
-hours = 40;
-pay = 23.43;
-totalreg = regwage(hours,pay);
-console.log("Regular Pay", totalreg); 
-
 function otwage(othours,otpay){
 	var totalot;
 	totalot = othours * otpay;
 	return totalot;
     }
-var hoursO;
-var payO;
-var totalot;
-hoursO = 7;
-payO = 1.5 * pay;
-totalot = otwage(hoursO,payO);
-console.log("Over Time Pay", totalot);
-
 function totalwage(regwage,otwage){
 	var result;
 	result = regwage + otwage;
 	return result;
 	}
-var wageA;
-var wageB;
-var combinedwage;
-wageA = regwage(hours,pay);
-wageB = otwage(hoursO,payO);
-combinedwage = totalwage(wageA,wageB);
-console.log("Total Pay",combinedwage);
+function main(){
+	var hours;
+	var pay;
+	var totalreg;
+	var hoursO;
+	var payO;
+	var totalot;
+	var wageA;
+	var wageB;
+	var combinedwage;
+	hours = 40;
+	pay = 23.43;
+	totalreg = regwage(hours,pay);
+	console.log("Regular Pay", totalreg);
+	hoursO = 7;
+	payO = 1.5 * pay;
+	totalot = otwage(hoursO,payO);
+	console.log("Over Time Pay", totalot);
+	wageA = regwage(hours,pay);
+	wageB = otwage(hoursO,payO);
+	combinedwage = totalwage(wageA,wageB);
+	console.log("Total Pay",combinedwage);
+}
+main();
