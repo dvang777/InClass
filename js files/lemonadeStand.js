1b2c15b9d60958c84ad2ab9e73c39aa1f79c3257
Daniel Vang <danielvang@ghalliance.org>
2016-01-11 17:21:53 -0600

    adjustment to lemonade stand

--- js files/lemonadeStand.js
+++ js files/lemonadeStand.js
@@ -24,23 +24,23 @@ function getWeather(weatherCondition){
 	}
 }
 function getNewTemperature(temperature){
-	    switch(new temperature().getTemperature()){
-	    case 1:
-	    temperature = Math.floor((Math.random()*99) + 85);
-	    console.log(temperature);
-	    break;
-	    case 2:
-	    temperature = Math.floor((Math.random()*84)+72);
-	    console.log(temperature);
-	    break;
-	    case 3:
-	    temperature = Math.floor((Math.random()*70)+ 66);
-	    console.log(temperature);
-	    break;
-	    case 4:
-	    temperature = Math.floor((Math.random()*65)+ 56);
-	    console.log(temperature);	
-    }
+	switch(new temperature().getTemperature()){
+		case 1:
+		temperature = Math.floor((Math.random()*99) + 85);
+		console.log(temperature);
+		break;
+		case 2:
+		temperature = Math.floor((Math.random()*84)+72);
+		console.log(temperature);
+		break;
+		case 3:
+		temperature = Math.floor((Math.random()*70)+ 66);
+		console.log(temperature);
+		break;
+		case 4:
+		temperature = Math.floor((Math.random()*65)+ 56);
+		console.log(temperature);	
+	}
 }
 function getDays(numberOfDays){
 	var forecast;
@@ -60,6 +60,11 @@ function getSupplies(){
 	lemon = 0;
 	sugar = 0;
 	ice = 0;
+}
+function getCustomers(numberOfCustomers){
+	var randomCustomers;
+	randomCustomers = Math.floor((Math.random()*150) + 1);
+	return randomCustomers;
 }
 function main(){
 	var NumberOfDays = [];
