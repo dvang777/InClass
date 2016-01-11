4e0b84498d09d95648a77401a9c8d061833a42fd
Daniel Vang <danielvang@ghalliance.org>
2016-01-11 08:28:08 -0600

    initial commit

--- js files/romanNumerals.js
+++ js files/romanNumerals.js
@@ -6,18 +6,29 @@ function getUserInput(message){
 	return userInput;
 }
 
+function convertNumber(number){
+	var numberInput = "";
+    var romanCharacter = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    var num = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    
+    for(var i=0; i<num.length; i++){
+        while(number>=num[i]){
+            numberInput = numberInput + romanCharacter[i];
+            number = number - num[i];
+        }
+    }
+    return numberInput;
+    }
 
 function main(){
     var userDataInput;
+    var romanNumeral;
+
+    userDataInput = getUserInput("Enter Data:");
+    romanNumeral = convertNumber(userDataInput);
+    console.log(romanNumeral);
 
 
-    userDataInput = getUserInput("Enter Number:")
-
-	while (isNaN(userDataInput)){
-		alert("User Input Error!");
-		userDataInput = getUserInput("Enter Number:");
-        
-    }
 }
 
 main();
