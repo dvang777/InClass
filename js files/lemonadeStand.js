10c7cf3ddcca3e011be373607b6fb136c91c3126
Daniel Vang <danielvang@ghalliance.org>
2016-01-11 16:39:12 -0600

    initial commit

---
+++ js files/lemonadeStand.js
@@ -0,0 +1,66 @@
+"use strict";
+function getUserInput(message){
+	var userInput;
+	userInput = window.prompt(message);
+	return userInput;
+}
+function getWeather(weatherCondition){
+	switch(new weatherCondition().getCondition()){
+		case 1:
+		condition = "Clear & Sunny";
+		break;
+		case 2:
+		condition = "Cloudy";
+		break;
+		case 3:
+		condition = "Rainy";
+		break;
+		case 4;
+		condition = "Overcast";
+		break;
+		case 5:
+		condition = "Hazy";
+		break;
+	}
+}
+function getNewTemperature(temperature){
+	    switch(new temperature().getTemperature()){
+	    case 1:
+	    temperature = Math.floor((Math.random()*99) + 85);
+	    console.log(temperature);
+	    break;
+	    case 2:
+	    temperature = Math.floor((Math.random()*84)+72);
+	    console.log(temperature);
+	    break;
+	    case 3:
+	    temperature = Math.floor((Math.random()*70)+ 66);
+	    console.log(temperature);
+	    break;
+	    case 4:
+	    temperature = Math.floor((Math.random()*65)+ 56);
+	    console.log(temperature);	
+    }
+}
+function getDays(numberOfDays){
+	var forecast;
+	var forecastDays = [];
+	for(forecast = 0; forecast < numberOfDays; forecast++){
+		forecastDays[forecast] = Math.floor((Math.random() * 31) + 1);
+	}
+	return boardLevels;   
+} 
+function getSupplies(){
+	var cups;
+	var lemon;
+	var sugar;
+	var ice;
+
+	cups = 0;
+	lemon = 0;
+	sugar = 0;
+	ice = 0;
+}
+function main(){
+	var NumberOfDays = [];
+}
--- js files/romanNumerals.js
+++ js files/romanNumerals.js
@@ -18,14 +18,32 @@ function convertNumber(number){
         }
     }
     return numberInput;
+}
+
+function convertRoman(roman){
+    var romanInput = "";
+    var romanCharacter = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    var num = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+
+    for (var i = 0; i >= romanCharacter[i]; i--) {
+        while(roman >= romanCharacter[i]){
+            romanInput = romanInput - romanCharacter[i];
+            roman = roman + num[i];
+        }
     }
+    return romanInput;
+}
 
 function main(){
     var userDataInput;
     var romanNumeral;
-
+    var numeral;
+    
+    
     userDataInput = getUserInput("Enter Data:");
     romanNumeral = convertNumber(userDataInput);
+    console.log(romanNumeral);
+    numeral = convertRoman(userDataInput);
     console.log(romanNumeral);
 
 
